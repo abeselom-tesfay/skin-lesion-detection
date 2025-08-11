a54dcb26f500120def90fd65bff7f754b80242e4
abeselom-ab <abeselomtesfay@gmail.com>
2025-08-11 19:23:48 +0300

    Implement Flask API for skin lesion prediction with Grad-CAM heatmap

--- src/model/serve.py
+++ src/model/serve.py
@@ -2,33 +2,35 @@ from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-import io
 from src.model.model import get_gradcam_heatmap
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('skin_lesion_model.h5')
+
+model = tf.keras.models.load_model('../../models/skin_lesion_model.keras')
 class_names = ['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC']
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
         return jsonify({'error': 'No file uploaded'}), 400
-    
+
     file = request.files['file']
-    img = Image.open(file).resize((224, 224))
+    img = Image.open(file).resize((224, 224)).convert('RGB')
     img_array = np.array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
-    
+
     preds = model.predict(img_array)
     pred_class = class_names[np.argmax(preds)]
     pred_prob = float(np.max(preds))
-    
+    probabilities = preds[0].tolist()
+
     heatmap = get_gradcam_heatmap(model, img_array)
     heatmap = (heatmap * 255).astype(np.uint8).tolist()
-    
+
     return jsonify({
         'class': pred_class,
         'probability': pred_prob,
+        'probabilities': probabilities,
         'heatmap': heatmap
     })
 
